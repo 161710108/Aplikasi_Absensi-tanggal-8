5ea20fba76eeff2b08094c96762172f02cfa6504
161710108 <161710108@smkassalaambandung.sch.id>
2018-06-08 16:20:04 +0700

    tanggal 8

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use all;
+use App\Absensi;
+use App\Siswa;
+use App\Absensi;
+class UserController extends Controller
+{
+    
+}
